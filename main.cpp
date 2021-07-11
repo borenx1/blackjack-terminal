58dad61fb463b147de9a86469a30ccd2da580753
Boren Xue <53597966+borenx1@users.noreply.github.com>
2021-07-11 17:09:48 +1200

    Add game setting constants

--- main.cpp
+++ main.cpp
@@ -3,9 +3,16 @@
 
 using namespace std;
 
+const double MIN_BET = 10;
+const double BET_INCREMENT = 1;
+const double STARTING_CASH = 1000;
+
 int main() {
     string name;
-    cout << "Now playing Blackjack" << endl;
+    cout << "Now playing Blackjack!" << endl;
+    cout << "You start with $" << STARTING_CASH << "." << endl;
+    cout << "The minimum bet is $" << MIN_BET << "." << endl;
+    cout << "Bets can only be in multiples of $" << BET_INCREMENT << "." << endl;
     cout << "Enter your name:" << endl;
     cin >> name;
     cout << "Your name is: " << name << endl;
