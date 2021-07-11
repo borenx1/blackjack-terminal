e8dff00e1eb22f40abaf67bf5209a3b97def74c3
Boren Xue <53597966+borenx1@users.noreply.github.com>
2021-07-11 18:21:32 +1200

    Create Blackjack class

---
+++ blackjack.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace blackjack {
+    struct BlackjackSettings {
+        double starting_cash;
+        double min_bet;
+        double bet_increment;
+        BlackjackSettings(double starting_cash = 1000, double min_bet = 10, double bet_increment = 1)
+            : starting_cash(starting_cash), min_bet(min_bet), bet_increment(bet_increment) {}
+    };
+
+    class Blackjack {
+        public:
+            Blackjack() {}
+
+            Blackjack(BlackjackSettings settings): settings(settings) {}
+
+            void start() {
+                printStartingMessage();
+                string name;
+                cout << "Enter your name:" << endl;
+                cin >> name;
+                cout << "Your name is: " << name << endl;
+            }
+        protected:
+            void printStartingMessage() {
+                cout << "Now playing Blackjack!" << endl;
+                cout << "You start with $" << settings.starting_cash << "." << endl;
+                cout << "The minimum bet is $" << settings.min_bet << "." << endl;
+                cout << "Bets can only be in multiples of $" << settings.bet_increment << "." << endl;
+            }
+        private:
+            BlackjackSettings settings;
+    };
+}
--- main.cpp
+++ main.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
 #include <string>
+#include "blackjack.cpp"
 
 using namespace std;
 
-const double MIN_BET = 10;
-const double BET_INCREMENT = 1;
-const double STARTING_CASH = 1000;
-
 int main() {
-    string name;
-    cout << "Now playing Blackjack!" << endl;
-    cout << "You start with $" << STARTING_CASH << "." << endl;
-    cout << "The minimum bet is $" << MIN_BET << "." << endl;
-    cout << "Bets can only be in multiples of $" << BET_INCREMENT << "." << endl;
-    cout << "Enter your name:" << endl;
-    cin >> name;
-    cout << "Your name is: " << name << endl;
+    blackjack::Blackjack game;
+    game.start();
 
     return 0;
 }
