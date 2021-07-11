6b7c1c281452eb620d7b6b255b35329a8271fcc9
Boren Xue <53597966+borenx1@users.noreply.github.com>
2021-07-11 16:59:50 +1200

    Create initial template

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Now playing Blackjack" << endl;
+    cout << "Enter your name:" << endl;
+    cin >> name;
+    cout << "Your name is: " << name << endl;
+
+    return 0;
+}
