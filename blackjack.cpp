34b4d437db1fdfc1c8ef83fc016f6ffdba117b1e
Boren Xue <53597966+borenx1@users.noreply.github.com>
2021-07-13 21:48:03 +1200

    Add Blackjack state struct

--- blackjack.cpp
+++ blackjack.cpp
@@ -9,8 +9,16 @@ namespace blackjack {
         double starting_cash;
         double min_bet;
         double bet_increment;
-        BlackjackSettings(double starting_cash = 1000, double min_bet = 10, double bet_increment = 1)
+        unsigned int decks;
+        BlackjackSettings(double starting_cash = 1000, double min_bet = 10, double bet_increment = 1, unsigned int decks = 4)
             : starting_cash(starting_cash), min_bet(min_bet), bet_increment(bet_increment) {}
+    };
+
+    struct State {
+        unsigned int round;
+        int cash;
+        card::Card hand[20];
+        card::Card deck[52 * 20];
     };
 
     class Blackjack {
