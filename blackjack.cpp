d0c3e2bd17d13d936d6d8947d5042a1cb0762f71
Boren Xue <53597966+borenx1@users.noreply.github.com>
2021-08-18 20:20:36 +1200

    Format `blackjack`
    Format code according the to Google C++ Style Guide.

--- blackjack.cpp
+++ blackjack.cpp
@@ -1,39 +1,43 @@
 #include "blackjack.h"
+
 #include <iostream>
 #include <string>
 
 namespace blackjack {
-    Blackjack::Blackjack() {
-        initState();
-    };
-    Blackjack::Blackjack(Settings settings): settings(settings) {
-        initState();
-    }
-    void Blackjack::start() {
-        printStartingMessage();
-        std::string name;
-        std::cout << "Enter your name:" << '\n';
-        std::cin >> name;
-        std::cout << "Your name is: " << name << '\n';
-    }
-    void Blackjack::restart() {
-        std::cout << '\n' << "New game" << '\n';
-        initState();
-        start();
-    }
-    void Blackjack::initState() {
-        // Create new state struct
-        State newState{};
-        newState.cash = settings.starting_cash;
-        newState.shoe = card::Shoe(settings.decks);
-        state = newState;
-        // Init shoe
-        state.shoe.refresh();
-    }
-    void Blackjack::printStartingMessage() {
-        std::cout << "Now playing Blackjack!" << '\n';
-        std::cout << "You start with $" << settings.starting_cash << "." << '\n';
-        std::cout << "The minimum bet is $" << settings.min_bet << "." << '\n';
-        std::cout << "Bets can only be in multiples of $" << settings.bet_increment << "." << '\n';
-    }
+
+Blackjack::Blackjack(Settings settings): settings_(settings) {
+    initState();
+}
+
+void Blackjack::start() {
+    printStartingMessage();
+    std::string name;
+    std::cout << "Enter your name:" << '\n';
+    std::cin >> name;
+    std::cout << "Your name is: " << name << '\n';
+}
+
+void Blackjack::restart() {
+    std::cout << '\n' << "New game" << '\n';
+    initState();
+    start();
+}
+
+void Blackjack::initState() {
+    // Create new state struct
+    State newState{};
+    newState.cash = settings_.starting_cash;
+    newState.shoe = card::Shoe(settings_.decks);
+    state_ = newState;
+    // Init shoe
+    state_.shoe.refresh();
+}
+
+void Blackjack::printStartingMessage() {
+    std::cout << "Now playing Blackjack!" << '\n';
+    std::cout << "You start with $" << settings_.starting_cash << "." << '\n';
+    std::cout << "The minimum bet is $" << settings_.min_bet << "." << '\n';
+    std::cout << "Bets can only be in multiples of $" << settings_.bet_increment << "." << '\n';
+}
+
 }
--- blackjack.h
+++ blackjack.h
@@ -1,37 +1,39 @@
 #ifndef BLACKJACK_H
 #define BLACKJACK_H
+
 #include "card.h"
 #include <string>
 #include <vector>
 
 namespace blackjack {
-    struct Settings {
-        double starting_cash{1000};
-        double min_bet{10};
-        double bet_increment{1};
-        int decks{4};
-    };
 
-    struct State {
-        int round{1};
-        double cash{};
-        std::vector<card::Card> hand{};
-        card::Shoe shoe{};
-    };
+struct Settings {
+    double starting_cash{1000};
+    double min_bet{10};
+    double bet_increment{1};
+    int decks{4};
+};
 
-    class Blackjack {
-        private:
-            Settings settings;
-            State state;
-        protected:
-            void initState();
-            void printStartingMessage();
-        public:
-            Blackjack();
-            Blackjack(Settings settings);
-            void start();
-            void restart();
-    };
+struct State {
+    int round{1};
+    double cash{};
+    std::vector<card::Card> hand{};
+    card::Shoe shoe{};
+};
+
+class Blackjack {
+    private:
+        Settings settings_;
+        State state_;
+    protected:
+        void initState();
+        void printStartingMessage();
+    public:
+        Blackjack(Settings settings = Settings{});
+        void start();
+        void restart();
+};
+
 }
 
 #endif
